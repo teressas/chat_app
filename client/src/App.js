db440ab522db8ad4fff56e0e6a91162f37c6a020
Teressa Sung <teressas.hawaii@gmail.com>
2022-08-09 20:28:18 -0700

    worked on client and server socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,88 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 import './App.css';
+import io from 'socket.io-client';
+import React, { useState, useEffect } from 'react';
 import Chat from '../src/components/Chat';
+
+/* alternative way to connect to socket.io-client
+remove socket dependencies setMessages useEffect call */
+// const socket = io.connect("http://localhost:8000");
 
 function App() {
 
   // notice that we pass a callback function to initialize the socket
   // we don't need to destructure the 'setSocket' function since we won't be updating the socket state
   const [socket] = useState(() => io(':8000'));
+  
+  //Room State
+  const [room, setRoom] = useState("");
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  // const [messagesList, setMessageList] = useState([]); // displays all messages
+
+  // useEffect(() => {
+  //   // we need to set up all of our event listeners
+  //   // in the useEffect callback function
+  //   console.log('Is this running?');
+  //   socket.on('Welcome', data => console.log(data));
+
+  //   // note that we're returning a callback function
+  //   // this ensures that the underlying socket will be closed if App is unmounted
+  //   // this would be more critical if we were creating the socket in a subcomponent
+  //   return () => socket.disconnect(true);
+  // }, [socket]);
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
 
   useEffect(() => {
-    // we need to set up all of our event listeners
-    // in the useEffect callback function
-    console.log('Is this running?');
-    socket.on('Welcome', data => console.log(data));
+    socket.on("receive_message", (data) => {
+      // alert(data.message),
+      // setMessageList((list)=> {
+      //   // console.log(data)
+      //   return [...list, data];
+      // })
+      setMessageReceived(data.message)
+    }
 
-    // note that we're returning a callback function
+    );
+
     // this ensures that the underlying socket will be closed if App is unmounted
     // this would be more critical if we were creating the socket in a subcomponent
-    return () => socket.disconnect(true);
+    // return () => socket.disconnect(true);
   }, [socket]);
+
 
   return (
     <div className="App">
       <h1>Socket Test</h1>
-      <Chat/>
+      <Chat />
+      <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <br/>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,40 +1,58 @@
 // server.js
 const express = require('express');
 const app = express();
+// socket.io is created upon an http server, recommended way in documentation: https://socket.io/docs/v4/server-initialization/#with-express
+const http = require('http');
+const { Server } = require("socket.io");
+const cors = require('cors');
 
-// req is shorthand for request
-// res is shorthand for response
-// app.get("/api", (req, res) => {
-//     res.json({ message: "Hello World" });
-// });
+app.use(cors());
 
-const server = app.listen(8000, () =>
-    console.log('The server is all fired up on port 8000')
-);
+const server = http.createServer(app);
 
 // To initialize the socket, we need to
 // invoke the socket.io library
 // and pass it our Express server
-const io = require('socket.io')(server, { cors: true });
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 
-io.on('connection', socket => {
+// const io = require('socket.io')(server, { cors: true });
+
+// We add all of our additional event listeners right inside this function.
+// NOTE "connection" is a BUILT IN event listeners.
+io.on('connection', (socket) => {
     // NOTE: Each client that connects get their own socket id!
-    console.log(socket.id, "", "Nice to meet you 🤝 ");
-    socket.emit("hello","world")
     // if this is logged in our node terminal, that means a new client has successfully completed the handshake!
-    // We add all of our additional event listeners right inside this function.
-    // NOTE "connection" is a BUILT IN event listeners.
+    console.log("A client connected:", socket.id);
+    //     socket.emit("Nice to meet you 🤝 ")
 
     /* socket.on will listen for an event from the client.
     It takes a callback function that contains the data from the client
     We then send that data straight to all the other clients.*/
-    socket.on("event_from_client", data => {
-        /* socket.broadcast will emit to all other clients besides the client who is actually emitting
+
+    // join a room and listens for room sent from client
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    });
+
+    socket.on("send_message", (data) => { // from send message funct on client
+        console.log(data)
+        socket.to(data.room).emit("receive_message", data); // specify room and then send msg
+        // socket.to(data.room).emit("receive_message", data);
+    });
+});
+/* socket.broadcast will emit to all other clients besides the client who is actually emitting
         Other events: 
         1. io.emit - emits an event to all connected clients
         2. socket.emit - emits an event directly to this specific client
         3. socket.broadcast.emit - emits an event to all clients other than this particular one, referenced by the socket variable
         */
-        socket.broadcast.emit("send_data_to_all_other_clients", data);
-    });
-});
+
+
+server.listen(8000, () =>
+    console.log('The server is all fired up on port 8000')
+);
